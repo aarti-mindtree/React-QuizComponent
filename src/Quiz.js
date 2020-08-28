6c78d6a4da35d6a1ffa72a9a1ffdcc5da86a5c9c
Aarti Kalekar <aarti.mindtree01@gmail.com>
2020-08-28 16:59:05 +0530

    Displaying Quiz Questions and Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,10 @@ class Quiz extends Component {
 
         this.state = { quiz_position: 1 };
     }
+
+    showNextQuestion() {
+        this.setState((prevState) => ({ quiz_position: prevState.quiz_position + 1 }))
+    }
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         return (
@@ -17,7 +21,9 @@ class Quiz extends Component {
                 {isQuizEnd 
                         ? <QuizEnd /> 
                         : <QuizQuestion 
-                            quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}  />}
+                            quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                            showNextQuestionHandler={this.showNextQuestion.bind(this)} 
+                />}
                 
             </div>
         );
